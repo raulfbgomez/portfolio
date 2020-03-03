fa962a58b421672af4163acc29ef531b349c92ce
Raul Gomez <raul_nouni@hotmail.com>
2020-03-03 00:45:31 -0600

    Add Google Analytics Tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,6 @@
-import Link from 'next/link'
 import Head from 'next/head'
+
+import { GA_TRACKING_ID } from '../lib/gtag.js'
 
 const Layout = (props) => {
   return (
@@ -8,6 +9,23 @@ const Layout = (props) => {
       <title>{ props.title }</title>
       <link href="https://fonts.googleapis.com/css?family=Spartan&display=swap" rel="stylesheet" />
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
     </Head>
     { props.children }
     </>
